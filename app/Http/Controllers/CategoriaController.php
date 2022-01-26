fb349c28e72e1302631519d5bf2ac097207ea87d
Marcos André <marcosandreazevedo78@gmail.com>
2022-01-26 00:20:01 -0300

    Início de validação de categoria e controller de categoria.

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Redirect;
+use App\Categoria;
+use App\Http\Request\CategoriaFormRequest;
+use DB;
+
 class CategoriaController extends Controller
 {
-    //
+    public function __construct(){
+
+    }
+
+    public function index(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }    
 }
