47d38fffe6d9033e17fef59245a5dd2b76b78e16
Marcos André <marcosandreazevedo78@gmail.com>
2022-01-26 00:20:01 -0300

    Início de validação de categoria e controller de categoria.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('estoque/categoria', 'CategoriaController');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
