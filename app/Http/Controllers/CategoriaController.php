97a5d8abc0764761cceb775f4c60f490011b00e8
Marcos André <marcosandreazevedo78@gmail.com>
2022-01-28 00:46:29 -0300

    Desenvolvimento de Controller de Categorias.

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -28,23 +28,48 @@ class CategoriaController extends Controller
         }
     }
 
-    public function store(){
+    public function create("estoque.categoria.create"){
 
     }
 
-    public function show(){
+    public function store(CategoriaFormRequest $request){
+        $categoria = new Categoria;
+        
+        $categoria->nome = $request->get('nome');
+        $categoria->descricao = $request->get('descricao');
+        $categoria->condicao = 1;
+        
+        $categoria->save();
 
+        return Redirect::to('categoria/estoque');
     }
 
-    public function edit(){
-
+    public function show($id){
+        return view("estoque.categoria.show", ["categoria" = Categoria::findOrFail(&id)]);
     }
 
-    public function update(){
-
+    public function edit($id){
+        return view("estoque.categoria.edit", ["categoria" = Categoria::findOrFail(&id)]);
     }
 
-    public function destroy(){
+    public function update(CategoriaFormRequest $request, $id){
+        $categoria = Categoria::findOrFail($id);
+        
+        $categoria->nome = $request->get('nome');
+        $categoria->descricao = $request->get('descricao');
+        
+        $categoria->update();
 
+        return Redirect::to('estoque/categoria');
+    }
+
+    public function destroy($id){
+        $categoria = Categoria::findOrFail($id);
+        
+        $categoria->condicao = '0';
+        
+        $categoria->update();
+
+        return Redirect::to('estoque/categoria');
     }    
 }
