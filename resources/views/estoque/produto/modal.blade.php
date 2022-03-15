bc2ed2b637baf3bdec47e69de6b91a7fd4494c20
Marcos André <marcosandreazevedo78@gmail.com>
2022-03-15 14:01:47 -0300

    Atualização na parte de imagem de produto.

--- resources/views/estoque/produto/modal.blade.php
+++ resources/views/estoque/produto/modal.blade.php
@@ -26,3 +26,25 @@
 	</div>
 	{{Form::Close()}}
 </div>
+
+<div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tabindex="-1" id="modal-imagem">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">×</span>
+                </button>
+				<h4>Visualizar Imagem</h4>
+			</div>
+			<div class="modal-body">
+				<img src="{{ asset('imagens/produtos/'.$produto->imagem_produto) }}" alt="{{ $produto->nome_produto }}" width="100%" heigth="100%" class="img-thumbnail">
+			</div>
+			<div class="modal-footer">
+				<button type="submit" class="btn btn-primary" data-dismiss="modal">
+					Confirmar
+					<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
