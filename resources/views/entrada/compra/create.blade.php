b11e340dd7d98544273b39a39d5ff9f08a0b88df
Marcos André <marcosandreazevedo78@gmail.com>
2022-04-18 21:40:35 -0300

    Refatoração create compra.

--- resources/views/entrada/compra/create.blade.php
+++ resources/views/entrada/compra/create.blade.php
@@ -80,21 +80,21 @@
 					<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
 						<div class="form-group">
 							<label for="num_doc">Quantidade</label>
-							<input type="number" name="quantidade" value="{{ old('quantidade') }}" id="quantidade" class="form-control" placeholder="Quantidade...">
+							<input type="number" name="quantidade" min="1" value="{{ old('quantidade') }}" id="quantidade" class="form-control" placeholder="Quantidade...">
 						</div>
 					</div>
 
 					<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
 						<div class="form-group">
 							<label for="num_doc">Preço Compra</label>
-							<input type="number" name="preco_compra" value="{{old('preco_compra')}}" id="preco_compra" class="form-control" placeholder="Preço de Compra...">
+							<input type="number" name="preco_compra" min="0" value="{{old('preco_compra')}}" id="preco_compra" class="form-control" placeholder="Preço de Compra...">
 						</div>
 					</div>
 
 					<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
 						<div class="form-group">
 							<label for="num_doc">Preço Venda</label>
-							<input type="number" name="preco_venda" value="{{ old('preco_venda') }}" id="preco_venda" class="form-control" placeholder="Preço de Venda...">
+							<input type="number" name="preco_venda" min="0" value="{{ old('preco_venda') }}" id="preco_venda" class="form-control" placeholder="Preço de Venda...">
 						</div>
 					</div>
 
@@ -127,18 +127,20 @@
 						</div>
 					</div>
 				</div>
-            </div>
+			</div>
 
-			<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12" id="divSalvarCancelar">
+			<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
 				<div class="form-group">
 					<input name="_token" value="{{ csrf_token() }}" type="hidden">
-                  	<button class="btn btn-primary" id="salvar" type="submit">
-					  	<i class="fa fa-floppy-o" aria-hidden="true"></i>
+					<button class="btn btn-primary" id="botaoSalvar" type="submit">
+						<i class="fa fa-floppy-o" aria-hidden="true"></i>
 						Salvar
 					</button>
-                  	<button class="btn btn-danger" type="reset">
-					  	<i class="fa fa-ban" aria-hidden="true"></i>
-					  	Cancelar
+					<button class="btn btn-danger" type="reset">
+						<i class="fa fa-ban" aria-hidden="true"></i>
+						<a href="{{ url('entrada/compra') }}" style="color:white;">
+							Cancelar
+						</a>
 					</button>
 				</div>
 			</div>
@@ -148,30 +150,30 @@
 		<script>
 			$(document).ready(function(){
 				$('#botaoAdicionar').click(function(){
-					adicionar();
+					adicionarTabelaEntrada();
 				});
 			});
 
-			var cont = totalEntrada = 0;
+			var contador = totalEntrada = 0;
 			subtotal = [];
 
-			$("#divSalvarCancelar").hide();
+			$("#botaoSalvar").hide();
 
-			function adicionar(){
+			function adicionarTabelaEntrada(){
 				idproduto = $("#id_produto").val();
 				produto = $("#id_produto option:selected").text();
 				quantidade = $("#quantidade").val();
 				preco_compra = $("#preco_compra").val();
 				preco_venda = $("#preco_venda").val();
 				
-				if(idproduto != "" && quantidade != "" && quantidade > 0 && preco_compra != "" && preco_venda != ""){
-					subtotal[cont] = quantidade * preco_compra;
-					totalEntrada += subtotal[cont];
+				if(idproduto != "" && quantidade != "" && quantidade > 0 && preco_compra != "" && preco_compra >= 0 && preco_venda != "" && preco_venda >= 0){
+					subtotal[contador] = quantidade * preco_compra;
+					totalEntrada += subtotal[contador];
 					
 					var linhaTabela = 
-						'<tr class="selected" id="linhaTabela'+cont+'">'+
+						'<tr class="selected" id="linhaTabela'+contador+'">'+
 							'<td>'+
-								'<button type="button" class="btn btn-danger" onclick="apagar('+cont+');">X</button>'+
+								'<button type="button" class="btn btn-danger" onclick="apagar('+contador+');">X</button>'+
 							'</td>'+
 							'<td>'+
 								'<input type="hidden" name="idproduto[]" value="'+idproduto+'">'+
@@ -187,11 +189,11 @@
 								'<input type="number" name="preco_venda[]" value="'+preco_venda+'">'+
 							'</td>'+
 							'<td>'+
-								subtotal[cont]+
+								subtotal[contador]+
 							'</td>'+
 						'</tr>';
 					
-					cont++;
+					contador++;
 					
 					limparCampos();
 					
@@ -212,9 +214,9 @@
 
 			function ocultarBotaoSalvar(){
 				if(totalEntrada > 0)
-					$("#divSalvarCancelar").show();
+					$("#botaoSalvar").show();
 				else
-					$("#divSalvarCancelar").hide();
+					$("#botaoSalvar").hide();
 			}
 
 			function apagar(index){
